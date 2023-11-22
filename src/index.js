a87742221c0f35379c9e5071efa1413468f89fb1
vifiehruth <vifiehruth@gmail.com>
2023-11-22 08:50:11 +0100

    added humidity wind and description

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 function displayTemperature(response) {
+  console.log(response)
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#windSpeed");
+ let iconElement = document.querySelector("#icon");
+ let descriptionElement = document.querySelector("#description");
+
+ iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon alt=${response.data.condition.icon}"/>`
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  descriptionElement.innerHTML = response.data.condition.description
 }
 
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-
+function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
@@ -44,10 +50,18 @@ function formatDate(date) {
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
+function handleSearchCity(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  searchCity(city);
+}
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSearchCity);
 
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+searchCity("Paris")
